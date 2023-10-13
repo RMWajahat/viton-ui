c2f337e7c6aa89adf94fe603a3b81b36ebc436cb
RMWajahat <rajawajahat2003@gmail.com>
2023-10-13 16:17:40 -0700

    viton new feature2 edited

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ export const Contact = () => {
         <h1>Contact us</h1>
         <small>We love to serve you</small>
       </div>
-<form className="form wid-80">
+<form className="form wid-80" action=''>
   <div className="title">Where we connect<br /><span>Contact</span></div>
   <input type="email" placeholder="Email" name="email" className="input" required />
   <textarea type="text" placeholder="Message" name="password" className="input" required/>
